44a6b3432335357af49abc9f35e01de357736dea
prajwal-006 <120716113+prajwal-006@users.noreply.github.com>
2023-01-17 18:57:31 +0530

    Add files via upload

---
+++ guess the number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+ int main(){
+     int number, guess, nguess=1;
+     srand(time(0));
+     number = rand()%100 + 1; // generates a random number between 1 and 100
+     do
+     {
+        printf("guess the number between 1 and 100\n");
+        scanf("%d",&guess);
+        if (guess>number)
+        {
+            printf("lower number please!\n");
+        }
+        else if (guess<number)
+        {
+             printf("higher number please!\n");
+        }
+        else
+        {
+            printf("you guessed it in %d attempts\n",nguess);
+        }
+        nguess++;
+     } while(guess!=number);
+
+     return 0;
+ }
